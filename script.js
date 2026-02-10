77f92bcb9de793ccbc9cc78d14fefc11274b9c31
Devil1716 <devil1716@users.noreply.github.com>
2026-02-10 04:52:50 -0800

    Update config to use 5 user photos

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const CONFIG = {
     partnerName: 'My Love',
     billboardPhotos: [
         'photos/photo1.jpg', 'photos/photo2.jpg', 'photos/photo3.jpg',
-        'photos/photo4.jpg', 'photos/photo5.jpg', 'photos/photo6.jpg'
+        'photos/photo4.jpg', 'photos/photo5.jpg'
     ]
 };
 
